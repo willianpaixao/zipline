a6415507adf5837011614e2e41b6e5aff3ce259f
Scott Sanderson <ssanderson@quantopian.com>
2018-08-15 16:35:53 -0400

    BUG: Fix circular import.

--- zipline/pipeline/__init__.py
+++ zipline/pipeline/__init__.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
 
 from .classifiers import Classifier, CustomClassifier
-from .engine import SimplePipelineEngine
 from .factors import Factor, CustomFactor
 from .filters import Filter, CustomFilter
 from .term import Term
 from .graph import ExecutionPlan, TermGraph
+# NOTE: this needs to come after the import of `graph`, or else we get circular
+# dependencies.
+from .engine import SimplePipelineEngine
 from .pipeline import Pipeline
-
 
 __all__ = (
     'Classifier',
